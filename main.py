b934b2ce8518349d359714b21cb6375c0df75897
nicolas-dalcorso <rdalcorso.nicolas@gmail.com>
2023-09-15 17:32:04 -0300

    Basic implementation of classes, dataclasses

--- main.py
+++
@@ -1,9 +0,0 @@
-import Modules.NaiveDataStructures as datas
-if __name__ == '__main__':
-    lista = datas.LinkedList();
-    
-    for i in range(1009):
-        lista.insert(str(i));
-        
-    for n in lista:
-        print(n.data);
